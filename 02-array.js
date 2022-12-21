49b303e7a0ddf16ce342bb0c13c815626acb5dce
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2022-12-21 16:22:41 -0300

    Desafio 02 completado

--- 02-array.js
+++ 02-array.js
@@ -1,11 +1,23 @@
+console.log("Desafio 02");
 // 1- Crie um array vazio chamado animals
+let animals = [];
 
 // 2- Adicione nesse array dois animais: dog e cat (você pode fazer isso de mais de uma forma diferente!)
+animals.push('dog', 'cat');
+console.log(animals);
 
 // 3- Adicione no começo do array outro animal: spider
+animals.unshift('spider');
+console.log(animals);
 
 // 4- Remova o primeiro elemento do array
-
+animals.shift();
+console.log(animals);
 // 5- Remova o último elemento do array
+animals.pop();
+console.log(animals);
 
 // 6- Como ficou o array final?! E qual é o elemento 1 desse array?
+console.log("O array final ficou assim: ");
+console.log(animals);
+console.log("O elemento 1 desse array é: " + animals[0]);
