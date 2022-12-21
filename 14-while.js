9cf36ab9ca64868385ed881452059ca4385c0be8
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2022-12-21 19:09:46 -0300

    Desafio 15 completado

--- 14-while.js
+++ 14-while.js
@@ -1,12 +1,12 @@
 // Utilizando o while, faça uma estrutura de repetição que dê o console dos números 50 a 100.
-console.log("Números de 50 a 100:")
+console.log("Números de 50 a 100:");
 let step = 50;
 while (step <= 100) {
     console.log(step);
     step++;
 }
 // Utilizando o while, faça uma estrutura de repetição que dê o console somente dos números pares, começando no 2 e indo até 100.
-console.log("Números pares de 2 a 100:")
+console.log("Números pares de 2 a 100:");
 let number = 2;
 while (number <= 100) {
     if (number % 2 == 0) {
@@ -14,8 +14,14 @@ while (number <= 100) {
     }
     number++;
 }
+console.log("Outra solução, pares de 2 a 100:");
+number = 2;
+while (number <= 100) {
+    console.log(number);
+    number += 2;
+}
 // Utilizando o while, faça uma estrutura de repetição que dê o console dos números 100 a 1.
-console.log("Números de 100 a 1:")
+console.log("Números de 100 a 1:");
 let inverse = 100;
 while (inverse >= 1) {
     console.log(inverse);
--- 15-for.js
+++ 15-for.js
@@ -1,5 +1,21 @@
 // Utilizando o for, faça uma estrutura de repetição que dê o console dos números 50 a 100.
-
+console.log("Números de 50 a 100:")
+for (let i = 50; i <= 100; i++) {
+    console.log(i);
+}
 // Utilizando o for, faça uma estrutura de repetição que dê o console somente dos números pares, começando no 2 e indo até 100.
-
+console.log("Pares de 2 a 100:");
+for (let i = 2; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+console.log("Outra solução, pares de 2 a 100:");
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
 // Utilizando o for, faça uma estrutura de repetição que dê o console dos números 100 a 1.
+console.log("Números de 100 a 1:")
+for (let i = 100; i >= 1; i--) {
+    console.log(i);
+}
