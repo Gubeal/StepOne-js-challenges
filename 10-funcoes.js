eeca6ff200602d3f1612901e0de93708b184f65f
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2022-12-21 18:25:52 -0300

    Desafio 10 completo

--- 10-funcoes.js
+++ 10-funcoes.js
@@ -5,3 +5,10 @@
 // A função deve mostrar no console a área do retângulo com a seguinte frase:
 // The area of rectangle is XXX.
 
+function rectangleArea(length, width) {
+    let area = length * width;
+    console.log(`The area of rectangle is ${area}.`);
+}
+
+console.log('Rectangle area of 10 and 20:');
+rectangleArea(10, 20);
