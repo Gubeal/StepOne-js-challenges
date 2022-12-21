ef37e4ca034fe579643c369951e86e832f2f3575
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2022-12-21 18:21:06 -0300

    Desafio 09 completado

--- 09-funcoes.js
+++ 09-funcoes.js
@@ -1,20 +1,30 @@
 // Crie as seguintes funções:
-
+let num1 = 10;
+let num2 = 2;
 // 1- Função de soma, com o nome ‘sum', que retorna a soma de dois números.
-
-
-
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(`Soma de ${num1} e ${num2}: ${sum(num1, num2)}`);
 
 // 2- Função de subtração, com o nome ‘subtract’, que retorna a subtração de dois números
-
-
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+console.log(`Subtração de ${num1} e ${num2}: ${subtract(num1, num2)}`);
 
 // 3- Função de multiplicação, com o nome ‘multiplication, que retorna 
 // a multiplicação de dois números
-
+function multiplication(num1, num2) {
+    return num1 * num2;
+}
+console.log(`Multiplicação de ${num1} e ${num2}: ${multiplication(num1, num2)}`);
 
 
 // 4- Função de divisão, com o nome 'division', que retorna a divisão do primeiro 
 //número pelo outro
 
-
+function division(num1, num2) {
+    return num1 / num2;
+}
+console.log(`Divisão de ${num1} por ${num2}: ${division(num1, num2)}`);
